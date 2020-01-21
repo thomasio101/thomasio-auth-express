f58740f9e64d3a10df6250852ed4dab7d0912923
Thomas Egbert Duursma <thomasduursma@outlook.com>
2020-01-21 19:14:25 +0100

    Implement user registration support.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { Request, RequestHandler } from 'express';
-import { ISession, SessionAuthenticator, UserAuthenticator } from 'thomasio-auth-js-common/lib/server';
+import {
+	IDatabaseInterface,
+	ISession,
+	SessionAuthenticator,
+	UserAuthenticator,
+	UserCreator,
+} from 'thomasio-auth-js-common/lib/server';
 
 export interface IRequestWithSession<T> extends Request {
 	session: ISession<T>;
@@ -32,6 +38,25 @@ export function loginHandler<T>(authenticator: UserAuthenticator<T>): RequestHan
 		if (typeof username === 'string' && typeof password === 'string') {
 			// TODO: Add post processing for sessions to, for example, remove internal data from the session's identity.
 			res.json(await authenticator(username, password));
+		} else {
+			res.status(400).send('Error from thomasio-auth-express.\nInvalid body.');
+		}
+	};
+}
+
+export type UserDataValidator<U> = (userData: U) => Promise<boolean>;
+
+export function registrationHandler<E, I, U>(
+	userCreator: UserCreator<E, I, U>,
+	userDataValidator: UserDataValidator<U>,
+): RequestHandler {
+	return async (req, res) => {
+		const { username, password, userData }: { username: any; password: any; userData: any } = req.body;
+
+		const userDataValidationPromise = userDataValidator(userData);
+
+		if (typeof username === 'string' && typeof password === 'string' && (await userDataValidationPromise)) {
+			res.json(await userCreator(username, password, userData));
 		} else {
 			res.status(400).send('Error from thomasio-auth-express.\nInvalid body.');
 		}
